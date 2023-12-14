2c1984ab2efb384fddaa00bf56266b595fd45885
Madhuri5859 <116060048+Madhuri5859@users.noreply.github.com>
2023-12-14 10:14:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,7 @@
-hi
+public class HelloWorld{
+  public static void main(String[] args){
+    system.out.println("Hello,World);
+    system.out.pritln("Continuous integration jenkins);
+    system.out.println("Maven Installation process");
+  }
+}
